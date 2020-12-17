2a9afcb88399e791436365de7214b30ce5196f09
yuvarajkarunakaran <44950024+yuvarajkarunakaran@users.noreply.github.com>
2020-12-17 13:12:49 +0530

    .

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Code1Component } from './code1/code1.component';
+import { PaymentComponent } from './payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'zipcode1/:pm', component:Code1Component},
+  {path:'pay',component:PaymentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
--- code1/code1.component.ts
+++ code1/code1.component.ts
@@ -9,15 +9,14 @@ import {ActivatedRoute} from '@angular/router';
 export class Code1Component implements OnInit {
 
   public zip:any;
+  id:any;
   constructor(private route:ActivatedRoute) { 
-    this.zip=this.route.snapshot.params.ip;
+   // this.zip=this.route.snapshot.params.ip;
   }
   
 
   ngOnInit(): void {
-  //   let ip=parseInt(this.route.snapshot.params['ip']);
-  //   alert(ip);
-  //  this.zip=ip;
+    this.id = this.route.snapshot.params['id'];
   }
 
 }
